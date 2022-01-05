80b55cffb5cb5ea8a577c6c562cae4056ab8f5d9
GorigeRaju1234 <87274610+GorigeRaju1234@users.noreply.github.com>
2022-01-05 15:52:20 +0530

    Create lambda_functions.py

---
+++ lambda_functions.py
@@ -0,0 +1,5 @@
+import keystonemiddleware
+#Using keystonemiddleware version 2.0 fir vulnerability test
+#Do nOT run this funciton, testing features of a security software for code scan
+def lambda_handler(event, context):
+    return "hello world"
